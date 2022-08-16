e8570b739118bbaf4b26ad8bb6bafdecf320ff66
De <spark49942@gmail.com>
2022-08-16 21:38:52 +0300

    Z1ppz2

--- Program.cs
+++ Program.cs
@@ -40,10 +40,38 @@ for (int imin = 0, imax = 0, j = 0, t = 0, bar = 0; bar<bnum; bar++)
     Console.WriteLine("");
 }
 }
-Z1();
+//Z1();
 
 void Z2() 
 {
-
+Random ra = new Random();
+int bnum = ra.Next(3, 12);
+int cnum = ra.Next(3, 12);
+int si = bnum*cnum;
+int [] ray = new int[si];
+int [,] table = new int[bnum, cnum];
+for (int i = 0, bar = 0; bar<bnum; bar++) 
+{
+    for (int col = 0; col<cnum; i++, col++)
+    {
+        int num = ra.Next(3, 12);
+        ray[i] = num;
+        Console.Write(ray[i] + "\t");
+    }
+    Console.WriteLine("");
+}
+Console.WriteLine();
+if (bnum == cnum)
+for (int temp = 0, i = 0, bar = 0; i<bnum; i++, bar++) 
+{
+    for (int j = i+1, col = 0; j<cnum; j++, col++)
+    {
+        temp = table[i, j];
+        table[i, j] = table[j, i];
+        table[j, i] = temp;
+        Console.Write(ray[i] + "\t");
+    }
+    Console.WriteLine("");
+}
 }
 Z2();
