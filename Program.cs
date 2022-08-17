b667531cfee5d7e07a730ed6da88b9678142ef11
De <spark49942@gmail.com>
2022-08-17 16:07:30 +0300

    DZ2-3f

--- Program.cs
+++ Program.cs
@@ -62,8 +62,9 @@ for (int i = 0, bar = 1; bar<bnum; bar++)
         if (col == cnum-1) Console.WriteLine("--> " + sum);
     }
     Console.WriteLine("");
-    if (bar == 1) { mins = sum; co++; }
-    else if (bar>1 && sum<mins) { mins = sum; co++; }
+    if (bar == 1) { mins = sum; comin = co + 1; }
+    else if (bar>1 && sum<mins) { mins = sum; comin = co + 1; }
+    co++;
 }
 Console.WriteLine("Строка с наименьшим значением суммы " + mins + " находится на " + co + " месте.");
 Console.WriteLine();
