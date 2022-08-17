3f25c41beb89565a712faaac4119205f6b669b30
De <spark49942@gmail.com>
2022-08-17 13:53:43 +0300

    DZ1

--- Program.cs
+++ Program.cs
@@ -17,109 +17,24 @@ for (int i = 0, bar = 0; bar<bnum; bar++)
     Console.WriteLine("");
 }
 Console.WriteLine();
-for (int imin = 0, imax = 0, j = 0, t = 0, bar = 0; bar<bnum; bar++) 
-{
-    for (int col = 0; col<cnum; j++, col++)
-    {
-        if(bar+1==bnum) 
-        {
-            //Console.Write(ray[i] + "\t");
 
-            imax++;
+int min = ray[0];
+//int max = ray[0];
+int tmp = 0;
+for (int i = 0, bar = 0; bar<bnum; bar++)
+{
+    for (int col = 0; col<cnum; i++, col++)
+    {
+        if (ray[i]<min) 
+        { 
+            tmp = min;
+            min = ray[i];
+            ray[i] = tmp;
         }
-        else if (bar>0) Console.Write(ray[j] + "\t");
-        if (bar==0) 
-        {
-            t = ray[imin];
-            ray[imin] = ray[imax];
-            ray[imax] = t;
-            Console.Write(ray[imin] + "\t");
-            Console.Write(ray[imax] + "\t");
-        }
+        Console.Write(ray[i] + "\t");
     }
     Console.WriteLine("");
 }
+
 }
 //Z1();
-
-void Z2() 
-{
-Random ra = new Random();
-Console.WriteLine("Введите равные значения строки и столбца");
-int bnum = Convert.ToInt32(Console.ReadLine());
-int cnum = Convert.ToInt32(Console.ReadLine());
-int si = bnum*cnum;
-int [] ray = new int[si];
-int [,] table = new int[bnum, cnum];
-for (int i = 0, bar = 0; bar<bnum; bar++) 
-{
-    for (int col = 0; col<cnum; i++, col++)
-    {
-        int num = ra.Next(3, 12);
-        ray[i] = num;
-        Console.Write(ray[i] + "\t");
-    }
-    Console.WriteLine("");
-}
-Console.WriteLine();
-if (bnum == cnum) 
-{
-for (int temp = 0, bar = 0; bar<bnum; bar++) 
-{
-    for (int col = bar+1; col<cnum; col++)
-    {
-        temp = table[bar, col];
-        table[bar, col] = table[col, bar];
-        table[col, bar] = temp;
-        Console.Write(ray[bar] + "\t");
-    }
-}
-for (int bar = 0; bar<bnum; bar++) 
-{
-    for (int j = 0, i = 0, col = 0; col<cnum; col++)
-    {
-        table[i, j] = ray[i];
-        Console.Write(table[i, j] + "\t");
-    }
-    Console.WriteLine("");
-}
-
-} else { Console.WriteLine("Замена строк и столбцов не представляется возможной."); }
-}
-//Z2();
-
-void Z3()
-{
-    Random ra = new Random();
-int bnum = ra.Next(3, 12);
-int cnum = ra.Next(3, 12);
-int si = bnum*cnum;
-int [] ray = new int[si];
-int [,] table = new int[bnum, cnum];
-for (int i = 0, bar = 0; bar<bnum; bar++) 
-{
-    for (int col = 0; col<cnum; i++, col++)
-    {
-        int num = ra.Next(3, 12);
-        ray[i] = num;
-        Console.Write(ray[i] + "\t");
-    }
-    Console.WriteLine("");
-//int[] ray = new int[10];
-}
-int[] count = new int[10];
-for(int bar = 0; bar < bnum; bar++) 
-{
-    for (int col = 0; col<cnum; col++) 
-    {
-        count[table[bar, col]]++;
-    }
-}
-Console.WriteLine();
-Console.WriteLine("Вывод ответа");
-for(int bar = 0; bar < si; bar++) 
-{
-    if (count[bar]> 0) Console.WriteLine($"число {bar} встречается {count[bar]} раз(-а)");
-}
-}
-Z3();
