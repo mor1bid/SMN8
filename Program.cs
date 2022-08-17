a111fa6605926b84d5a6c1edcf3e829fcc65c7dd
De <spark49942@gmail.com>
2022-08-17 14:59:02 +0300

    DZ1-2ex

--- Program.cs
+++ Program.cs
@@ -30,12 +30,51 @@ for (int i = 0, j = 0, bar = 0; bar<bnum; bar++)
             Console.Write(ray[i] + "\t");
             ray[i] = tmp;
         }
-        Console.Write(min + "\t");
         //Console.Write(ray[i] + "\t");
     }
     Console.WriteLine("");
     j += cnum;
 }
-
 }
-DZ1();
+Console.WriteLine("");
+
+void DZ2() 
+{
+Random ra = new Random();
+int bnum = 6;
+int cnum = 3;
+int si = bnum*cnum;
+int [] ray = new int[si];
+int [,] table = new int[bnum, cnum];
+int count = 0;
+for (int i = 0, bar = 0; bar<bnum; bar++) 
+{
+    for (int col = 0; col<cnum; i++, col++)
+    {
+        int num = ra.Next(3, 12);
+        ray[i] = num;
+        Console.Write(ray[i] + "\t");
+    }
+    Console.WriteLine("");
+}
+Console.WriteLine();
+int min = ray[0];
+for (int i = 0, j = 0, bar = 0; bar<bnum; bar++)
+{
+    for (int col = 0; col<cnum; i++, col++)
+    {
+        if (ray[i]<=min) 
+        { 
+            min = ray[i];
+            count++;
+        }
+        //Console.Write(ray[i] + "\t");
+    }
+    Console.WriteLine("");
+}
+Console.WriteLine("Строка с наименьшим значением " + min + " находится на " + count + "месте.");
+}
+
+//DZ1();
+DZ2();
+//DZ3();
