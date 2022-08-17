ab584c057cc243a61d5c62c68451c2a0a2400c01
De <spark49942@gmail.com>
2022-08-17 16:39:13 +0300

    DZ1-3

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ for (int i = 0, bar = 0; bar<bnum; bar++)
 Console.WriteLine();
 for (int i = 0, j = 0, bar = 0; bar<bnum; bar++)
 {
+    int max = ray[j];
     int min = ray[j];
     int tmp = 0;
     for (int col = 0; col<cnum; i++, col++)
@@ -27,8 +28,14 @@ for (int i = 0, j = 0, bar = 0; bar<bnum; bar++)
         { 
             tmp = min;
             min = ray[i];
-            Console.Write(ray[i] + "\t");
+            Console.Write(ray[i] + "");
             ray[i] = tmp;
+        }
+        if (ray[i]>=max)
+        {
+            tmp = max;
+            max = ray[i];
+            Console.WriteLine(ray[i] + "\t");
         }
         //Console.Write(ray[i] + "\t");
     }
