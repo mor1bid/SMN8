71f90bd7fd30aba782b14e39ff67bf3970ac4dda
De <spark49942@gmail.com>
2022-08-17 14:28:06 +0300

    DZ1-2ex

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ for (int i = 0, j = 0, bar = 0; bar<bnum; bar++)
         { 
             tmp = min;
             min = ray[i];
-            Console.Write(ray[i] + "\t");
+            Console.Write(min + "\t");
             ray[i] = tmp;
         }
         //Console.Write(ray[i] + "\t");
