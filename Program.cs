f1ac454bff5f3dbe4c731953b99957b3ba119b82
De <spark49942@gmail.com>
2022-08-17 14:12:50 +0300

    DZ1-2

--- Program.cs
+++ Program.cs
@@ -27,14 +27,13 @@ for (int i = 0, j = 0, bar = 0; bar<bnum; bar++)
     int tmp = 0;
     for (int col = 0; col<cnum; i++, col++)
     {
-        if (ray[i]<=min) 
+        if (ray[i]<min) 
         { 
             tmp = min;
             min = ray[i];
             Console.Write(ray[i] + "\t");
             ray[i] = tmp;
         }
-        Console.Write(ray[i] + "\t");
     }
     Console.WriteLine("");
     j += cnum;
