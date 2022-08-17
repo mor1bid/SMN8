261bac91fc59ef71a190fa29adaf61713c439d83
De <spark49942@gmail.com>
2022-08-17 16:59:11 +0300

    DZ1-42

--- Program.cs
+++ Program.cs
@@ -8,40 +8,41 @@ int [] ray = new int[si];
 int [,] table = new int[bnum, cnum];
 for (int i = 0, j = 0, bar = 0; bar<bnum; bar++) 
 {
-     int min = ray[j];
+    int min = ray[j];
     int tmp = 0;
     for (int col = 0; col<cnum; i++, col++)
     {
         int num = ra.Next(3, 12);
         ray[i] = num;
-        if (ray[i]<=min) 
-        { 
-            tmp = min;
-            min = ray[i];
-            Console.Write(ray[i] + "\t");
-            ray[i] = tmp;
-        }
-        else Console.Write(ray[i] + "\t");
+        // if (ray[i]<=min) 
+        // { 
+        //     tmp = min;
+        //     min = ray[i];
+        //     Console.Write(ray[i] + "\t");
+        //     ray[i] = tmp;
+        // }
+        Console.Write(ray[i] + "\t");
     }
     Console.WriteLine("");
     j += cnum;
 }
 Console.WriteLine();
-// for (int i = 0, j = 0, bar = 0; bar<bnum; bar++)
-// {
-//     // int max = ray[j];
-//     // int tmp2 = 0;
-//     int min = ray[j];
-//     int tmp = 0;
-//     for (int col = 0; col<cnum; i++, col++)
-//     {
-//         if (ray[i]<=min) 
-//         { 
-//             tmp = min;
-//             min = ray[i];
-//             Console.Write(ray[i] + " ");
-//             ray[i] = tmp;
-//         }
+ for (int i = 0, j = 0, bar = 0; bar<bnum; bar++)
+ {
+     // int max = ray[j];
+     // int tmp2 = 0;
+     int min = ray[j];
+     int tmp = 0;
+     for (int col = 0; col<cnum; i++, col++)
+     {
+        if (ray[i]<=min) 
+        { 
+            tmp = min;
+            min = ray[i];
+            Console.Write(ray[i] + " ");
+            ray[i] = tmp;
+        }
+        if (bar==bnum-1) { Console.Write(tmp + "\t"); }
 //         // if (ray[i]>=max)
 //         // {
 //         //     tmp2 = max;
@@ -52,7 +53,8 @@ Console.WriteLine();
 //     }
 //     Console.WriteLine("");
 //     j += cnum;
-// }
+}
+}
 }
 Console.WriteLine("");
 
