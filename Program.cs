02dd25a3af725c446f10fbcd67c859794f18847e
De <spark49942@gmail.com>
2022-08-17 23:47:23 +0300

    DZ1-44

--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ Console.WriteLine();
      }
      for (int i2 = 0, col = 0; col<cnum; i2++, col++)
      {
-        if (col == cnum-1 && ray[i2]>=min && ray[i2]<=max) 
+        if (col == cnum-1 && ray[i2]>=min) 
         { 
         Console.Write(ray[i2] + "\t");
         }
