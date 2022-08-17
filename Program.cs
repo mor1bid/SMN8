503ad6f2045786db2035f9d727ace863c619dd65
De <spark49942@gmail.com>
2022-08-18 01:18:45 +0300

    DZ1-5tab

--- Program.cs
+++ Program.cs
@@ -48,7 +48,7 @@ Console.WriteLine();
      }
      for (int i2 = 0, col = 0; col<cnum; i2++, col++)
      {
-        if (ray[i2]>=max) 
+        if (ray[i2]<=max) 
         {
         Console.Write(ray[i2] + "\t");
         }
