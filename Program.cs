938620db7ad01927a573ee4e7ec09b7fa6c96d32
De <spark49942@gmail.com>
2022-08-18 16:45:30 +0300

    DZ1-yhz<ex

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ for (int i = 0, bar = 0; bar<bnum-1; bar++)
     //Console.WriteLine("");
     //j += cnum;
 }
-for (int i = 0, bar = 0; bar<=bnum; bar++)
+for (int i = 0, bar = 0; bar<bnum; bar++)
 {
     for (int col = 0; col<=cnum; col++, i++)
     {
