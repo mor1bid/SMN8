4fc55c1312c114af3a2e68dcc22740a520fe726b
De <spark49942@gmail.com>
2022-08-18 15:17:27 +0300

    DZ1-yhz

--- Program.cs
+++ Program.cs
@@ -33,7 +33,7 @@ Console.WriteLine();
  {
     for (int col = 0; col<cnum; col++)
     {
-        Console.WriteLine(ray[col]);
+        Console.Write(ray[col]);
     }
  }
     Console.WriteLine("");
