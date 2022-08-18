601ec95e474c9b2a770b2b76cb72f50b7438626b
De <spark49942@gmail.com>
2022-08-18 16:32:58 +0300

    DZ1-yhz<ex

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ for (int i = 0, bar = 0; bar<bnum; bar++)
 Console.WriteLine();
 for (int i = 0, bar = 0; bar<bnum-1; bar++)
 {
+    int j = 0;
     for (int col = 0; col<cnum-1; col++, i++)
     {
         if (ray[i]< ray[i+1])
@@ -28,21 +29,21 @@ for (int i = 0, bar = 0; bar<bnum-1; bar++)
             ray[i+1] = tmp;
         }
     }
-    // for (int i = 0, col = 0; col<cnum; col++, i++)
-    // {
-    //     Console.Write(ray[i] + "\t");
-    // }
-    // Console.WriteLine("");
-}
-for (int i = 0, bar = 0; bar<bnum; bar++)
-{
-    for (int col = 0; col<cnum; col++, i++)
+    for (int col = 0; col<cnum; col++, j++)
     {
         Console.Write(ray[i] + "\t");
     }
-        Console.WriteLine("");
-//     //j+=cnum;
+    Console.WriteLine("");
+    j += cnum;
 }
+// for (int i = 0, bar = 0; bar<bnum; bar++)
+// {
+//     for (int col = 0; col<cnum; col++, i++)
+//     {
+//         Console.Write(ray[i] + "\t");
+//     }
+//         Console.WriteLine("");
+// }
 }
 
 void DZ2() 
